3543d85ed442f5251ad75a5e9032c385790441a6
Han Daerim <gkseofla7@naver.com>
2022-08-09 15:03:43 +0900

    BreakPoint

--- Source/PuzzlePlatforms/AnimInstance/SoldierAnimInstance.cpp
+++ Source/PuzzlePlatforms/AnimInstance/SoldierAnimInstance.cpp
@@ -3,3 +3,46 @@
 
 #include "SoldierAnimInstance.h"
 
+#include "../PuzzlePlatformsCharacter.h"
+USoldierAnimInstance::USoldierAnimInstance()
+{
+	CurrenPawnSpeed = 0.0f;
+}
+
+void USoldierAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	auto Pawn = TryGetPawnOwner();
+
+	if (::IsValid(Pawn))
+	{
+		CalculateDirection(Pawn->GetVelocity(), Pawn->GetActorRotation());
+		CurrenPawnSpeed = Pawn->GetVelocity().Size();
+		Direction = CalculateDirection(Pawn->GetVelocity(), Pawn->GetActorRotation());
+
+		FRotator ControlRotation = Pawn->GetControlRotation();
+		//float A = 360.0 - ControlRotation.Vector().X;
+		float A = 360.0 - ControlRotation.Pitch;
+		//float B = ControlRotation.Vector().Y* -1;
+		float B = ControlRotation.Pitch;
+		//float C = ControlRotation.Vector().Z;
+		float C = ControlRotation.Yaw;
+		float tmp = 0;
+		if (B >= 180)
+		{
+			tmp = A / 3;
+		}
+		else
+		{
+			tmp = B *(-1)/ 3;
+		}
+		AimRotation = FRotator(0, 0, tmp);
+		
+		//UE_LOG(LogTemp, Warning, TEXT("%f"), AimRotation.Vector().Y);
+		auto Character = Cast<ACharacter>(Pawn);
+		if (Character)
+		{
+			IsInAir = Character->GetMovementComponent()->IsFalling();
+		}
+	}
+}
--- Source/PuzzlePlatforms/AnimInstance/SoldierAnimInstance.h
+++ Source/PuzzlePlatforms/AnimInstance/SoldierAnimInstance.h
@@ -13,5 +13,22 @@ UCLASS()
 class PUZZLEPLATFORMS_API USoldierAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	USoldierAnimInstance();
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;//이건 Tick마다 호출하는 함수
+
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	float CurrenPawnSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		float Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		FRotator AimRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool IsInAir;
 };
