f60db55a0f7234eb6674d226cc40046e1d827194
Han Daerim <gkseofla7@naver.com>
2022-08-10 15:04:44 +0900

    Break Point

---
+++ Source/PuzzlePlatforms/Weapons/Object_Master.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object_Master.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AObject_Master::AObject_Master()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SkeletalMeshComponent =  CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+}
+
+// Called when the game starts or when spawned
+void AObject_Master::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObject_Master::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/PuzzlePlatforms/Weapons/Object_Master.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+
+#include "Object_Master.generated.h"
+
+UCLASS()
+class PUZZLEPLATFORMS_API AObject_Master : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObject_Master();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	class USkeletalMeshComponent * SkeletalMeshComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
