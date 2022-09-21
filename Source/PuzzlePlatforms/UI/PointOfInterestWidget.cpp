658834d12cee978568b216a79c7158a84781efaf
DaerimHan <gkseofla7@naver.com>
2022-09-21 14:35:25 +0900

    BreakPoint

---
+++ Source/PuzzlePlatforms/UI/PointOfInterestWidget.cpp
@@ -0,0 +1,102 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PointOfInterestWidget.h"
+#include "../PlayersComponent/PointOfInterestComponent.h"
+#include "../MyPlayerController.h"
+#include "../UI/PlayerInfoWidget.h"
+#include "../UI/MinimapWidget.h"
+
+#include "Components/Image.h"
+#include "Components/Throbber.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UPointOfInterestWidget::UPointOfInterestWidget(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	static ConstructorHelpers::FClassFinder<UActorComponent> PointOfInterestComponentBPClass(TEXT("/Game/MiniMap/BP_PointOfInterestComponent"));
+	if (PointOfInterestComponentBPClass.Succeeded())
+	{
+		PointOfInterestComponentClass = PointOfInterestComponentBPClass.Class;
+
+		//EquippedItem->Get
+	}
+}
+
+void UPointOfInterestWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	auto Controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	ABCHECK(Controller != nullptr);
+	auto MyController = Cast< AMyPlayerController>(Controller);
+	ABCHECK(MyController != nullptr);
+	if (MyController->PlayerInfoHUDWidget != nullptr)
+	{
+		auto MinimapWidget = MyController->PlayerInfoHUDWidget->Minimap_Widget;
+		if (MinimapWidget != nullptr)
+		{
+			auto Target = MyController->GetPawn();
+			ABCHECK(Target != nullptr);
+			ABCHECK(Owner != nullptr);
+			float MapDimensions = MinimapWidget->Zoom * (MinimapWidget->Dimensions / 300);
+			auto TargetLoc = Target->GetActorLocation();
+			auto OwnerLoc = Owner->GetActorLocation();
+			float deltaX = (TargetLoc.X - OwnerLoc.X)/MapDimensions;
+			float deltaY = -1*(TargetLoc.Y - OwnerLoc.Y)/MapDimensions;
+			FVector2D Point = FVector2D(deltaX, deltaY);
+			FVector2D PlayerPoint = FVector2D(0, 0);
+			float Angle = FindAngle(PlayerPoint, Point);
+			float Radius = Point.Size();
+			FVector2D CoordVec = FindCoord(Radius, Angle);
+			SetRenderTranslation(CoordVec);
+			if (IsStatic== false)
+			{
+				if (CoordVec.Size() >= 130.)
+				{
+					DefaultImage->SetVisibility(ESlateVisibility::Hidden);//이미지 있으면 이미지로 해야됨
+				}
+				else
+				{
+					DefaultImage->SetVisibility(ESlateVisibility::Visible);
+				}
+			}
+
+		}
+	}
+}
+
+void UPointOfInterestWidget::CustomInitialize(AActor* NewOwner, bool NewIsStatic)
+{
+	Owner = NewOwner;
+	IsStatic = NewIsStatic;
+	UActorComponent*  Comp = Owner->GetComponentByClass(PointOfInterestComponentClass);//걍bp말고 일반찾아도,,ㅋㅋ
+	ABCHECK(Comp != nullptr);
+	auto PointOfInterest = Cast<UPointOfInterestComponent>(Comp);
+	ABCHECK(PointOfInterest);
+	if (PointOfInterest->IconImage != nullptr)
+	{
+		CustomImage->SetBrushFromTexture(PointOfInterest->IconImage);
+		DefaultImage->SetVisibility(ESlateVisibility::Hidden);
+	}
+	else
+	{
+		CustomImage->SetVisibility(ESlateVisibility::Hidden);
+	}
+
+}
+
+float UPointOfInterestWidget::FindAngle(FVector2D A, FVector2D B)
+{
+	float DeltaX = A.X - B.X;
+	float DeltaY = A.Y - B.Y;
+	float angle = UKismetMathLibrary::Atan2(DeltaY, DeltaX);
+	return angle;
+}
+FVector2D UPointOfInterestWidget::FindCoord(float Radius, float Angle)
+{
+	float NewRadius = UKismetMathLibrary::Clamp(Radius, 0., 130.);
+	float CoordX = (-1)* NewRadius* UKismetMathLibrary::DegSin(Angle);
+	float CoordY = (-1) * NewRadius * UKismetMathLibrary::DegCos(Angle);
+	FVector2D Out = FVector2D(CoordX, CoordY);
+	return Out;
+}
---
+++ Source/PuzzlePlatforms/UI/PointOfInterestWidget.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../PuzzlePlatforms.h"
+#include "Blueprint/UserWidget.h"
+
+
+#include "PointOfInterestWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPointOfInterestWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPointOfInterestWidget(const FObjectInitializer& ObjectInitializer);
+	void CustomInitialize(AActor* NewOwner, bool NewIsStatic);
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	float FindAngle(FVector2D A, FVector2D B);
+	FVector2D FindCoord(float Radius, float Angle);
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* Owner;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool IsStatic;
+	TSubclassOf<UActorComponent> PointOfInterestComponentClass;
+	UPROPERTY(meta = (BindWidget),BlueprintReadWrite)
+		class UImage* CustomImage;
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite)
+		class UThrobber* DefaultImage;
+
+};
